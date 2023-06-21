bb5e079f00538b363f0b97189115fc53b51d1d11
Kyle Devereaux <devereauxk@berkeley.edu>
2023-06-21 17:56:10 +0200

    local push

--- slurm_O2_sim.sh
+++ slurm_O2_sim.sh
@@ -3,7 +3,7 @@
 #SBATCH -q regular
 #SBATCH -J O2_sim
 #SBATCH -t 02:00:00
-#SBATCH --output=/home/kdevereaux/ali_practice/slurm/slurm-%A_%a.out
+#SBATCH --output=/home/kdevereaux/ali_practice/slurm/slurm-%j.out
 
 #OpenMP settings:
 export OMP_NUM_THREADS=1
@@ -11,4 +11,4 @@ export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
 #run the application:
-srun -n 1 -c 256 --cpu_bind=cores /home/kdevereaux/hiccup_projects/process_O2_sim.sh
+srun -n 1 -c 256 /home/kdevereaux/hiccup_projects/process_O2_sim.sh
