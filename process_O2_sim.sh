705ea243f00358ce7c7f959a0ac0cca2b7a34e12
Kyle Devereaux <devereauxk@berkeley.edu>
2023-06-21 17:53:39 +0200

    local push

--- process_O2_sim.sh
+++ process_O2_sim.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-OUTDIR=/home/kdevereaux/ali_practice/slurm
+OUTDIR=/home/kdevereaux/hiccup_projects/ali_practice/
 cd $OUTDIR
 
 ~/o2-sim -n 5 -m PIPE ITS -g pythia8pp -e TGeant4 -j 2
