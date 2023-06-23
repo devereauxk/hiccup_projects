818a55638ea952b63a9b4fb6478a93c09e000ead
Kyle Devereaux <devereauxk@berkeley.edu>
2023-06-23 20:40:55 +0200

    local push

--- process_O2_sim.sh
+++ process_O2_sim.sh
@@ -3,6 +3,6 @@
 OUTDIR=/home/kdevereaux/hiccup_projects/ali_practice
 cd $OUTDIR
 
-source /home/kdevereaux/env.txt # full path of file containing appropriate variables
+source /home/kdevereaux/setup_O2_env.sh # full path of file containing appropriate variables
 
 o2-sim -n 2 -m PIPE ITS -g pythia8pp -e TGeant4 -j 2
--- slurm_O2_sim.sh
+++ slurm_O2_sim.sh
@@ -2,10 +2,8 @@
 #SBATCH --job-name=O2_sim
 #SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
 #SBATCH --partition=std
-#SBATCH --time=24:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=/home/kdevereaux/hiccup_projects/slurm-%j.out
-
-source /home/kdevereaux/env.txt
 
 #run the application:
 srun /home/kdevereaux/hiccup_projects/process_O2_sim.sh
