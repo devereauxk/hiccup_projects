4421ad7e6dbf070b4b8d080cdc730a9c47d7d78a
Kyle Devereaux <devereauxk@berkeley.edu>
2024-01-30 16:58:27 -0800

    perlmutter push

--- pp_alice_unfolding/unfold.C
+++ pp_alice_unfolding/unfold.C
@@ -4,10 +4,8 @@
 #include <TH3.h>
 #include <TSystem.h>
 #include <TString.h>
-//#include <RooUnfoldBayes.h>
-//#include <RooUnfoldResponse.h>
-
-gSystem->Load("libRooUnfold");
+#include </global/cfs/cdirs/alice/kdevero/RooUnfold/RooUnfold/build/RooUnfoldBayes.h>
+#include </global/cfs/cdirs/alice/kdevero/RooUnfold/RooUnfold/build/RooUnfoldResponse.h>
 
 // usage
 // root -q "unfold.C(\"preunfold.root\", \"unfolded.root\", 3)"
@@ -16,36 +14,36 @@ void unfold(const TString& infile="preunfold.root", const TString& outfile="unfo
     // Set ROOT to batch mode
     gROOT->SetBatch(true);
 
-    // Load the RooUnfold library
-    //gSystem->Load("libRooUnfold");
-
     // Error treatment for unfolding
     RooUnfold::ErrorTreatment errorTreatment = RooUnfold::kCovariance;
 
-    // Open input files
-    TFile* responseFile = TFile::Open(infile + ":reco_gen");
-    TFile* response1DFile = TFile::Open(infile + ":reco1D_gen1D");
-    TFile* h3_rawFile = TFile::Open(infile + ":raw");
-    TFile* h1_rawFile = TFile::Open(infile + ":raw1D");
+    // Open inputs
+    TFile* f = new TFile(infile);
+    RooUnfoldResponse* response = (RooUnfoldResponse*) f->Get("reco_gen");
+    RooUnfoldResponse* response1D = (RooUnfoldResponse*) f->Get("reco1D_gen1D");
+    TH3D* h3_raw = (TH3D*) f->Get("raw");
+    TH1D* h1_raw = (TH1D*) f->Get("raw1D");
 
     // Create output file
     TFile* fout = new TFile(outfile, "RECREATE");
 
     for (int i = 1; i <= iter; ++i) {
         // Unfold the 3D histogram
-        RooUnfoldResponse* response = (RooUnfoldResponse*)responseFile->Get("response");
-        TH3* h3_raw = (TH3*)h3_rawFile->Get("h3_raw");
         RooUnfoldBayes unfold(response, h3_raw, i);
-        TH3* hunf = (TH3*)unfold.Hreco(errorTreatment);
-        TH3* hfold = (TH3*)response->ApplyToTruth(hunf, "");
+        TH3* hunf = (TH3*)unfold.Hunfold(errorTreatment);
+        //TH3* hfold = (TH3*)response->ApplyToTruth(hunf, "");
+
+        cout<<"unfolded"<<endl;
 
         // Clone and name histograms
         TH3* htempUnf = (TH3*)hunf->Clone(TString::Format("Baysian_Unfoldediter%d", i));
-        TH3* htempFold = (TH3*)hfold->Clone(TString::Format("Baysian_Foldediter%d", i));
+        //TH3* htempFold = (TH3*)hfold->Clone(TString::Format("Baysian_Foldediter%d", i));
+
+        cout<<"written"<<endl;
 
         // Write histograms to output file
         htempUnf->Write();
-        htempFold->Write();
+        //htempFold->Write();
     }
 
     // Write output file
