a6c8c342d1e544c1c5b5d9182e22f7d279ce8a65
Kyle Devereaux <devereauxk@berkeley.edu>
2024-03-21 00:24:04 -0700

    perlmutter push

--- pp_alice_unfolding/construct_response.py
+++ pp_alice_unfolding/construct_response.py
@@ -4,15 +4,16 @@
 # WARNING METHODS APPEND TO OUTPUT FILE, WHEN RUNNING CONFIRM THIS IS INTENDED BEHAVIOR
 
 # usage - run with the "pyjetty_load" environment
-# CLOSURE
+# CLOSURE (pythia)
 # python3 construct_response.py --mc_file ../pp_unfolding/preprocess_sigma335_400k.root --data_file ../pp_unfolding/preprocess_sigma2_400k.root --output_file ./preunfold_closure_new.root --closure True
+# CLOSURE (fullsim)
+# python3 construct_response.py --mc_file /global/cfs/cdirs/alice/kdevero/pp_alice_unfolding/AnalysisResults/mc-13794540/merged_1star.root --data_file /global/cfs/cdirs/alice/kdevero/pp_alice_unfolding/AnalysisResults/mc-13794540/merged_1star.root --output_file ./preunfold_closurefullsim_redoredo.root --closure True
 # FULL SIM / DATA
-# python3 construct_response.py --mc_file /global/cfs/cdirs/alice/kdevero/pp_alice_unfolding/AnalysisResults/mc-13794540/merged_small.root --data_file /global/cfs/cdirs/alice/kdevero/pp_alice_unfolding/AnalysisResults/data-13796056/merged_medium.root --output_file ./preunfold_fr_new.root
+# python3 construct_response.py --mc_file /global/cfs/cdirs/alice/kdevero/pp_alice_unfolding/AnalysisResults/mc-13794540/merged_1star.root --data_file /global/cfs/cdirs/alice/kdevero/pp_alice_unfolding/AnalysisResults/data-13796056/merged.root --output_file ./preunfold_fr.root
 
 
 # imports
 import numpy as np
-import uproot as ur
 import ROOT
 import argparse
 ROOT.gSystem.Load("libRooUnfold.so")
@@ -26,18 +27,11 @@ binnings = [np.logspace(-5,0,n_bins[0]+1), \
             np.array([5, 20, 40, 60, 80, 100, 150]).astype(float) ]
 # using 8E-3 as the lower rl bin: solve 10^x = 8*10^(-3) 
 
-gen_features = ["gen_energy_weight", "gen_R_L", "gen_jet_pt"]
-obs_features = ["obs_energy_weight", "obs_R_L", "obs_jet_pt"]
-labels = ["energy weight", "$R_L$", "jet $p_T$"]
-
 def construct_response(n_mc_file="preprocessed_mc.root", n_out="preunfold.root"):
 
     print("constructing response matrix from mc_file ... ")
 
-    fout = ROOT.TFile(n_out, 'UPDATE')
-    fout.cd()
-
-    print(binnings)
+    if verbose > 1: print("binnings : " + str(binnings))
 
     h3_reco = ROOT.TH3D("reco", "reco", n_bins[0], binnings[0], n_bins[1], binnings[1], n_bins[2], binnings[2])
     h3_gen = ROOT.TH3D("gen", "gen", n_bins[0], binnings[0], n_bins[1], binnings[1], n_bins[2], binnings[2])
@@ -48,43 +42,43 @@ def construct_response(n_mc_file="preprocessed_mc.root", n_out="preunfold.root")
     response = ROOT.RooUnfoldResponse(h3_reco, h3_gen)
     response1D = ROOT.RooUnfoldResponse(h1_reco, h1_gen)
 
-    synth_tree = ur.open("%s:preprocessed"%(n_mc_file))
-    synth_df = synth_tree.arrays(library="pd")
+    print("loading synthetic tree...")
+    fin = ROOT.TFile.Open(n_mc_file)
+    synth_tree = fin.preprocessed
+    print("reading events ...")
 
-    if verbose > 1:
-        print("synthetic data")
-        print(synth_df.tail(10)) #look at some entries
-
-    all_features = gen_features + obs_features
-    theta0 = synth_df[all_features].to_numpy()
-
-    n_rows = len(theta0)
+    n_rows = synth_tree.GetEntries()
     i = 0
     last_jetpt_obs = -1
-    for row in theta0:
+    for row in synth_tree:
 
         i += 1
         if i % int(n_rows / 50) == 0: print("{} / {}".format(i, n_rows))
 
-        [weight_gen, rl_gen, jetpt_gen, weight_obs, rl_obs, jetpt_obs] = [i for i in row]
+        [weight_gen, rl_gen, jetpt_gen, weight_obs, rl_obs, jetpt_obs, pt_hat_weight] = [row.gen_energy_weight, row.gen_R_L, row.gen_jet_pt, row.obs_energy_weight, row.obs_R_L, row.obs_jet_pt, row.pt_hat_weight]
+        # remember to weigh everything with pt_hat_weight!
 
-        h3_gen.Fill(weight_gen, rl_gen, jetpt_gen)
+        h3_gen.Fill(weight_gen, rl_gen, jetpt_gen, pt_hat_weight)
 
         # if sucessful measurement, assumes missed are given some negative value for the energy weight
         if weight_obs >= 0:
-            response.Fill(weight_obs, rl_obs, jetpt_obs, weight_gen, rl_gen, jetpt_gen)
-            h3_reco.Fill(weight_obs, rl_obs, jetpt_obs)
+            response.Fill(weight_obs, rl_obs, jetpt_obs, weight_gen, rl_gen, jetpt_gen, pt_hat_weight)
+            h3_reco.Fill(weight_obs, rl_obs, jetpt_obs, pt_hat_weight)
         else:
-            response.Miss(weight_gen, rl_gen, jetpt_gen)
+            response.Miss(weight_gen, rl_gen, jetpt_gen, pt_hat_weight)
         
         # only fill 1D jetpt unfolding matrix once per jet
         # assumes perfect jet matching - is that valid ?!
         if jetpt_obs >= 0 and jetpt_obs != last_jetpt_obs:
-            response1D.Fill(jetpt_obs, jetpt_gen)
-            h1_reco.Fill(jetpt_obs)
-            h1_gen.Fill(jetpt_gen)
+            response1D.Fill(jetpt_obs, jetpt_gen, pt_hat_weight)
+            h1_reco.Fill(jetpt_obs, pt_hat_weight)
+            h1_gen.Fill(jetpt_gen, pt_hat_weight)
             last_jetpt_obs = jetpt_obs
-            
+    
+    fin.Close()
+
+    fout = ROOT.TFile(n_out, 'UPDATE')
+    fout.cd()
 
     response.Write()
     response1D.Write()
@@ -101,10 +95,7 @@ def construct_response(n_mc_file="preprocessed_mc.root", n_out="preunfold.root")
 def constructed_data_hist(n_data_file="preprocessed_data.root", n_out="preunfold.root", closure=False):
 
     print("constructing raw data histogram from natural data file ... ")
-    if closure: print("(clusure test) true natural histograms also prepared")
-    
-    fout = ROOT.TFile(n_out, 'UPDATE')
-    fout.cd()
+    if closure: print("(closure test) true natural histograms also prepared")
 
     h3_raw = ROOT.TH3D("raw", "raw", n_bins[0], binnings[0], n_bins[1], binnings[1], n_bins[2], binnings[2])
     h1_raw =  ROOT.TH1D("raw1D", "raw1D", n_bins[2], binnings[2])
@@ -117,46 +108,47 @@ def constructed_data_hist(n_data_file="preprocessed_data.root", n_out="preunfold
 
         h2_true_eec = ROOT.TH2D("true_eec", "true_eec", n_bins[1], binnings[1], n_bins[2], binnings[2])
 
-    natural_tree = ur.open("%s:preprocessed"%(n_data_file))
-    natural_df = natural_tree.arrays(library="pd")
+    print("loading natural tree...")
+    fin = ROOT.TFile.Open(n_data_file)
+    natural_tree = fin.preprocessed
+    print("reading events ...")
 
-    if verbose > 1:
-        print("synthetic data")
-        print(natural_df.tail(10)) #look at some entries
-
-    if closure:
-        all_features = gen_features + obs_features
-    else:
-        all_features = obs_features
-    theta_unknown = natural_df[all_features].to_numpy()
-
-    n_rows = len(theta_unknown)
+    n_rows = natural_tree.GetEntries()
     i = 0
     last_jetpt_obs = -1
-    for row in theta_unknown:
+    for row in natural_tree:
 
         i += 1
         if i % int(n_rows / 50) == 0: print("{} / {}".format(i, n_rows))
 
         if closure:
-            [weight_gen, rl_gen, jetpt_gen, weight_obs, rl_obs, jetpt_obs] = row
+            [weight_gen, rl_gen, jetpt_gen, weight_obs, rl_obs, jetpt_obs, pt_hat_weight] = [row.gen_energy_weight, row.gen_R_L, row.gen_jet_pt, row.obs_energy_weight, row.obs_R_L, row.obs_jet_pt, row.pt_hat_weight]
+
+            h3_raw.Fill(weight_obs, rl_obs, jetpt_obs, pt_hat_weight)
+            h2_raw_eec.Fill(rl_obs, jetpt_obs, weight_obs * pt_hat_weight)
+
+            h3_true.Fill(weight_gen, rl_gen, jetpt_gen, pt_hat_weight)
+            h2_true_eec.Fill(rl_gen, jetpt_gen, weight_gen * pt_hat_weight)
         else:
-            [weight_obs, rl_obs, jetpt_obs] = row
+            [weight_obs, rl_obs, jetpt_obs] = [row.obs_energy_weight, row.obs_R_L, row.obs_jet_pt]
 
-        h3_raw.Fill(weight_obs, rl_obs, jetpt_obs)
-        h2_raw_eec.Fill(rl_obs, jetpt_obs, weight_obs)
-
-        if closure:
-            h3_true.Fill(weight_gen, rl_gen, jetpt_gen)
-            h2_true_eec.Fill(rl_gen, jetpt_gen, weight_gen)
+            h3_raw.Fill(weight_obs, rl_obs, jetpt_obs)
+            h2_raw_eec.Fill(rl_obs, jetpt_obs, weight_obs)
         
         # only fill 1D jetpt unfolding matrix once per jet
         if jetpt_obs >= 0 and jetpt_obs != last_jetpt_obs:
-            h1_raw.Fill(jetpt_obs)
             last_jetpt_obs = jetpt_obs
 
             if closure:
-                h1_true.Fill(jetpt_gen)
+                h1_raw.Fill(jetpt_obs, pt_hat_weight)
+                h1_true.Fill(jetpt_gen, pt_hat_weight)
+            else:
+                h1_raw.Fill(jetpt_obs)
+
+    fin.Close()
+
+    fout = ROOT.TFile(n_out, 'UPDATE')
+    fout.cd()
 
     h3_raw.Write()
     h1_raw.Write()
@@ -164,7 +156,7 @@ def constructed_data_hist(n_data_file="preprocessed_data.root", n_out="preunfold
     if closure:
         h3_true.Write()
         h1_true.Write()
-        h2_raw_eec.Write()
+        h2_true_eec.Write()
 
     fout.Write()
     fout.Close()
@@ -184,5 +176,5 @@ if __name__ == "__main__":
 
     print("closure ; ", flags.closure)
 
-    construct_response(flags.mc_file, flags.output_file)
+    # construct_response(flags.mc_file, flags.output_file)
     constructed_data_hist(flags.data_file, flags.output_file, flags.closure)
--- pp_alice_unfolding/unfold.C
+++ pp_alice_unfolding/unfold.C
@@ -10,11 +10,13 @@
 // usage
 // CLOSURE
 // root -q "unfold.C(\"preunfold_closure_new.root\", \"unfolded_closure_new.root\", 5)"
+// CLOSURE WITH FULLSIM
+// root -q "unfold.C(\"preunfold_closurefullsim.root\", \"unfolded_closurefullsim.root\", 5)"
 // FULLSIM / DATA
-// root -q "unfold.C(\"preunfold_fr_new.root\", \"unfolded_fr_new.root\", 5)"
+// root -q "unfold.C(\"preunfold_closurefullsim.root\", \"preunfold_fr.root\", \"unfolded_fr.root\", 5)"
 
 
-void unfold(const TString& infile="preunfold.root", const TString& outfile="unfolded.root", int iter=9) {
+void unfold(const TString& rm_file="preunfold.root", const TString& data_file="preunfold_data.root", const TString& outfile="unfolded.root", int iter=9) {
     // Set ROOT to batch mod
     gROOT->SetBatch(true);
 
@@ -22,13 +24,13 @@ void unfold(const TString& infile="preunfold.root", const TString& outfile="unfo
     RooUnfold::ErrorTreatment errorTreatment = RooUnfold::kCovariance;
 
     // Open inputs
-    TFile* f = new TFile(infile);
+    TFile* f = new TFile(rm_file);
     RooUnfoldResponse* response = (RooUnfoldResponse*) f->Get("reco_gen");
     RooUnfoldResponse* response1D = (RooUnfoldResponse*) f->Get("reco1D_gen1D");
-    TH3D* h3_raw = (TH3D*) f->Get("raw");
-    TH1D* h1_raw = (TH1D*) f->Get("raw1D");
-
-    // TODO add in h1 unfolding
+    TFile* f_data = new TFile(data_file);
+    TH3D* h3_raw = (TH3D*) f_data->Get("raw");
+    TH1D* h1_raw = (TH1D*) f_data->Get("raw1D");
+    // do not close these, if think its because the program reads directly from the files and does NOT copy it into memory
 
     // Create output file
     TFile* fout = new TFile(outfile, "RECREATE");
@@ -70,23 +72,27 @@ void unfold(const TString& infile="preunfold.root", const TString& outfile="unfo
 }
 
 int main(int argc, char* argv[]) {
-    TString input_file = "preunfold.root";
+    TString rm_file = "preunfold.root";
+    TString data_file = rm_file;
     TString output_file = "unfolded.root";
-    int iter = 9;
+    int iter = 5;
 
     // Parse command line arguments if provided
     if (argc > 1) {
-        input_file = argv[1];
+        rm_file = argv[1];
     }
     if (argc > 2) {
-        output_file = argv[2];
+        data_file = argv[2];
     }
     if (argc > 3) {
-        iter = std::atoi(argv[3]);
+        output_file = argv[3];
+    }
+    if (argc > 4) {
+        iter = std::atoi(argv[4]);
     }
 
     // Run unfolding
-    unfold(input_file, output_file, iter);
+    unfold(rm_file, data_file, output_file, iter);
 
     return 0;
 }
